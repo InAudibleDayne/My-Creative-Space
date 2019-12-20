19c4aae3aff31c8a65eff89fad2035d9d218088d
InAudibleDayne <daynebechtold@hotmail.com>
2019-12-20 12:40:07 -0700

    created header component and styled

---
+++ src/components/Header/header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+        <div className='header'>
+            <div className='header__links-wrapper'>
+                <div className='header__links'>
+                    Music
+                </div>
+                <div className='header__links'>
+                    Videos
+                </div>
+                <div className='header__links'>
+                    Books
+                </div>
+                <div className='header__links'>
+                    Art
+                </div>
+            </div>
+        </div>
+    );
+  }
+}

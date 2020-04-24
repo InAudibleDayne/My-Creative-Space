6d72240ef33642df013cb32f5fcfa4706ffc5910
InAudibleDayne <daynebechtold@hotmail.com>
2020-04-24 12:35:33 -0600

    added facebook login option for login page

--- src/Helpers/icons.js
+++ src/Helpers/icons.js
@@ -1,8 +1,9 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faPhone, faEnvelope, faMapMarkedAlt, faLock } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
 
 const Icons = () => {
-    return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faPhone, faEnvelope, faMapMarkedAlt, faLock);
+    return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faPhone, faEnvelope, faMapMarkedAlt, faLock, faFacebook);
 };
 
 export default Icons;
--- src/components/Auth/login.js
+++ src/components/Auth/login.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import GoogleLogin from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from 'axios';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 export default class Login extends Component {
     constructor(props) {
@@ -16,6 +18,9 @@ export default class Login extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.successResponseGoogle = this.successResponseGoogle.bind(this);
+        this.responseGoogle = this.responseGoogle.bind(this);
+        this.responseFacebook = this.responseFacebook.bind(this);
+        this.componentClicked = this.componentClicked.bind(this);
       }
     
       handleChange(event) {
@@ -55,6 +60,27 @@ export default class Login extends Component {
           });
           this.props.handleSuccessfulAuth();
       }
+
+      componentClicked(){
+        console.log("clicked")
+      }
+
+      responseFacebook = (response) => {
+        var userName = response.id;
+        var firstName = response.name;
+        axios({method: 'post',
+              url: 'http://localhost:5000/user',
+              data: {username: `${userName}`, name: `${firstName}`}})
+              .then(response => {
+                var userId = response.data.id;
+                var loginName = response.data.name;
+                this.props.handleSuccessfulAuth(loginName, userId);
+              })
+              .catch(error => {
+                console.log("Login Error", error);
+              });
+      }
+
   render() {
     return (
         <div>
@@ -67,10 +93,29 @@ export default class Login extends Component {
                 <GoogleLogin
                   className="google-login__btn"
                   clientId="239390937938-vqum64b7vba20bvof5qjsm5a6e24ve4v.apps.googleusercontent.com"
-                  buttonText="Login"
+                  buttonText="Login with Google"
                   onSuccess={this.successResponseGoogle}
                   onFailure={this.responseGoogle}
                   cookiePolicy={'single_host_origin'}
+                />
+              </div>
+              <div className='separater'>
+                <h2>
+                  OR
+                </h2>
+              </div>
+              <div className="facebook-login">
+                <h2>
+                  Login with Facebook
+                </h2>
+                <FontAwesomeIcon icon={faFacebook} className='facebook-login__logo' />
+                <FacebookLogin
+                  appId="253159762538046"
+                  autoLoad={true}
+                  fields="name,email,picture"
+                  onClick={this.componentClicked}
+                  callback={this.responseFacebook}
+                  cssClass="facebook-login__btn"
                 />
               </div>
         </div>
